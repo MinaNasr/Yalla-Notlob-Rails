5f9401e21bfab46d46e1e9e7b9d514a707eb1489
raniaSayed <raniasayed313@gmail.com>
2018-04-11 00:32:07 +0200

    add delete method to allowed methods

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module YallaNotlob
      config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post,:delete :options]
       end
     end
     #autoloads lib folder during production
